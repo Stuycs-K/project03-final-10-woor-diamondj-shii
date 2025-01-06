8ce765a606576be5908a0a09ebaad66bfba9f2f9
Ronnie Woo <rwoo50@stuy.edu>
2025-01-06 15:29:45 -0500

    created skeleton files

---
+++ control.h
@@ -0,0 +1,11 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+/*
+Creates word and file to store guesses
+*/
+void gameSetup();
+/*
+Takes a 5-letter string, prints color-coded result
+*/
+void checkGuess(char* word);
+#endif
---
+++ game.h
@@ -0,0 +1,7 @@
+#ifndef GAME_H
+#define GAME_H
+/*
+Prints the game board with all previous results and underscores for remaining guesses, or a blank screen if it's the first turn
+*/
+void printBoard();
+#endif
