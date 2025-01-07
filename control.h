8ba8948f316205b06c4905e91ca747ed9330db7c
Ronnie Woo <rwoo50@stuy.edu>
2025-01-07 15:07:14 -0500

    update skeleton

--- control.h
+++ control.h
@@ -4,8 +4,4 @@
 Creates word and file to store guesses
 */
 void gameSetup();
-/*
-Takes a 5-letter string, prints color-coded result
-*/
-void checkGuess(char* word);
 #endif
--- game.h
+++ game.h
@@ -4,4 +4,8 @@
 Prints the game board with all previous results and underscores for remaining guesses, or a blank screen if it's the first turn
 */
 void printBoard();
+/*
+Takes a 5-letter string, prints color-coded result
+*/
+void checkGuess(char* word);
 #endif
