09714a55f52a48e4e58a282b0c59a74f679695fa
Ian Shi <Ianshi1026@gmail.com>
2025-01-08 15:35:47 -0500

    generateRandomWords returns char* of length 6 now

--- control.c
+++ control.c
@@ -20,7 +20,7 @@ char * generateRandomWord() {
   FILE * file = fopen("valid_answers.txt", "r");
   srand(time(NULL));
   int targetWord = rand() % TOTAL_ANSWERS;
-  char * word = malloc(8);
+  char word[8];
 
   int i = 1;
   while (fgets(word, 8, file)) {
@@ -35,12 +35,17 @@ char * generateRandomWord() {
       exit(1);
     }
   }
-  printf("target word: %d\n", targetWord);
+
+  char * shortenedWord = malloc(6);
+  strncpy(shortenedWord, word, 5);
+  shortenedWord[5] = '\0';
   
-  return word;
+  return shortenedWord;
 }
 
 void gameSetup() {
+  char * word = generateRandomWord();
+  printf("word: %s\n", word);
   // create semaphore w/ value 1
   int semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
   union semun us;
