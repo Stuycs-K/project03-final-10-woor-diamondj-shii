5d165ac1d2ecab9f63d4d060c47859bdebca88c7
Julien Diamond <jdiamond50@stuy.edu>
2025-01-09 15:14:26 -0500

    merging

--- game.c
+++ game.c
@@ -1,3 +1,32 @@
- int main() {
+#define SEMKEY 826534
 
- }
+union semun {
+  int val;                  //used for SETVAL
+  struct semid_ds *buf;     //used for IPC_STAT and IPC_SET
+  unsigned short  *array;   //used for SETALL
+  struct seminfo  *__buf;
+};
+
+int main() {
+  while (1) {
+    // access semaphore
+    printf("waiting for turn...\n");
+    int semd = semget(SEMKEY, 1, 0);
+    struct sembuf sb;
+    sb.sem_num = 0;
+    sb.sem_flg = SEM_UNDO;
+    sb.sem_op = -1;
+
+    // decrement semaphore
+    semop(semd, &sb, 1);
+
+    // simulating turn
+    printf("taking turn (3 seconds)\n");
+    sleep(3);
+    printf("turn over\n");
+
+    // increment semaphore
+    sb.sem_op = 1;
+    semop(semd, &sb, 1);
+  }
+}
