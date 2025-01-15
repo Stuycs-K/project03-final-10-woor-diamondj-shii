496778d4eae7ed4a94c03e874f24b014a54b52ae
Julien Diamond <juliendiamond@Juliens-MacBook-Air.local>
2025-01-15 14:28:05 -0500

    copied play.c main into game.c runGame() to begin file coalition

--- game.c
+++ game.c
@@ -66,35 +66,58 @@ void checkGuess(char* guess, char* answer){
 }
 
 void runGame(int semkey, int shmkey) {
+  //initialize global variables
+  char* answer = (char*) malloc(6 * sizeof(char));
+  int win = 0;
   while (1) {
-    printf("runGame called with semkey = %d & shmkey = %d\n", semkey, shmkey);
-
-    // access semaphore
+    // wait to access semaphore
     printf("waiting for turn...\n");
-    int semd = semget(semkey, 1, 0);
+    int semd = semget(SEMKEY, 1, 0);
     struct sembuf sb;
     sb.sem_num = 0;
     sb.sem_flg = SEM_UNDO;
     sb.sem_op = -1;
-    printf("semaphore accessed\n");
 
-    // decrement semaphore
+    // decrement semaphore once accessed
     semop(semd, &sb, 1);
 
-    // accessing shared data
-    printf("accessing shared data...\n");
-    char* answer = (char*) malloc(6 * sizeof(char));
-    int shmid = shmget(shmkey, 0, 0);
+    //access char* answer
+    int shmid = shmget(ANSWERSHMKEY, 0, 0);
     answer = shmat(shmid, 0, 0);
-    printf("shared data accessed\n");
 
-    // simulating turn
-    printf("answer is %s\n", answer);
-    printf("taking turn (3 seconds)\n");
-    sleep(3);
-    printf("turn over\n");
+    //access guess file
+    int guessFile = open("guesses.txt", O_RDWR | O_APPEND);
 
-    // increment semaphore
+    // making turn
+    printBoard();
+    char buffer[BUFFERSIZE] = {'\0'};
+    printf("Enter a 5-letter word.\n");
+    fgets(buffer, BUFFERSIZE, stdin);
+    *(strchr(buffer, '\n')) = '\0';
+    while (isValidGuess(buffer) == 0){
+      printf("Invalid guess. Please enter a 5-letter word.\n");
+      fgets(buffer, BUFFERSIZE, stdin);
+      *(strchr(buffer, '\n')) = '\0';
+    }
+
+    //check if answer is guessed
+    if (strcmp(buffer, answer) == 0){
+      win = 1;
+    }
+    checkGuess(buffer, answer);
+    *(strchr(buffer, '\0')) = '\n';
+    write(guessFile, buffer, strlen(buffer));
+    printBoard();
+
+    //print message if answer is guessed
+    if (win == 1){
+      printf("You guessed the word!\n");
+    }
+
+    //detach shared memory
+    shmdt(answer);
+    
+    // release and increment semaphore
     sb.sem_op = 1;
     semop(semd, &sb, 1);
   }
